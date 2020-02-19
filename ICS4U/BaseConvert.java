93c56a5f952a2154cab44f81b0f470b9f626e9ed
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2020-02-19 09:23:44 -0500

    Update BaseConvert.java

--- ICS4U/BaseConvert.java
+++ ICS4U/BaseConvert.java
@@ -3,7 +3,7 @@ public class BaseConvert {
   public static String valueToString(int value){
     String back = "ABCDEF";
     
-    return value > 9 ? back.charAt(value-10) : value;
+    return value > 9 ? back.charAt(value-10) + "" : "" + value;
     
   }
   
@@ -13,13 +13,14 @@ public class BaseConvert {
      return valueToString(value); 
     }
     
-    return decToBase(value % base, base) + valueToString(value/base);
+    return valueToString(value/base) + decToBase(value % base, base);
     
   }
   
   public static void main(String[]args){
      
-     
+     System.out.println(decToBase(16,8));
+
    }
 
 
